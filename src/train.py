5d15bfeec11c696ba83c38aa01a39903a002ced6
Maxim Degi <unliex@yandex.ru>
2026-07-02 12:14:01 +0300

    trained the model

--- src/train.py
+++ src/train.py
@@ -5,12 +5,14 @@ from ultralytics import YOLO
 MODELS = ["yolo26s.pt", "yolo26l.pt"]
 DEVICE = "cuda"
 RESULT_DIR = Path("training_results")
-DATA_PATH = Path("dataset/VisDrone.yaml")
+YOLO_YAML_PATH = Path("dataset/BoneFractureYolo8/data.yaml")
 RESULT_DIR.mkdir(exist_ok=True)
 for model_name in MODELS:
     # Load a model
     model = YOLO(model_name)
 
     # Train the model
-    results = model.train(data=DATA_PATH, epochs=100, device=DEVICE)
-    json.dump(results, open(str(RESULT_DIR / model_name) + ".json", "w+"), indent="\t")
+    results = model.train(data=YOLO_YAML_PATH, epochs=100, device=DEVICE)
+    json.dump(
+        dict(results), open(str(RESULT_DIR / model_name) + ".json", "w+"), indent="\t"
+    )
